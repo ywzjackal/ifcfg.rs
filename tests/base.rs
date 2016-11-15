3e9ef5d14f7e32663cbc6989944595cfdf2a2e9f
Jackal <ywzjackal@163.com>
2016-11-15 14:57:43 +0800

    fix test test_get_ipv4byname

--- tests/base.rs
+++ tests/base.rs
@@ -5,18 +5,6 @@ extern crate ifcfg;
 
 #[test]
 fn test_get_ipv4byname() {
-    let mut ifni = ifcfg::ffi::ifreq_addr::new();
-    unsafe {
-        let fd: libc::c_int = libc::socket(libc::AF_INET, libc::SOCK_DGRAM, libc::IPPROTO_IP);
-        assert!(fd >= 0);
-        ifni.ifr_name.set("lo");
-        let rt: libc::c_int = libc::ioctl(fd, ifcfg::ffi::SIOCGIFADDR, &mut ifni);
-        assert_eq!(rt, 0);
-        assert_eq!(ifni.ifr_addr.get_family(), 2);
-        assert_eq!(ifni.ifr_addr.get_port(), 0);
-        assert_eq!(ifni.ifr_addr.get_addr(),
-                   (127 << 0) | (0 << 8) | (0 << 16) | (1 << 24));
-    }
-
-    assert_eq!(ifcfg::get_ipv4byname("lo").unwrap(), Ipv4Addr::from((127 << 0) | (0 << 8) | (0 << 16) | (1 << 24)));
+    assert_eq!(ifcfg::get_ipv4byname("lo").unwrap(),
+               Ipv4Addr::new(127, 0, 0, 1));
 }
